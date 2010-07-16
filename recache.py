54a8735b0a8a1e1d03b36df08403aa5c344b92b2
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2010-07-16 22:33:54 +0200

    Use 'lib/extralibs' instead of 'extralibs' (#124).

--- recache.py
+++ recache.py
@@ -4,8 +4,8 @@ import os
 import os.path
 import fileinput
 
-DIR = "E:\\ghc\\ghc-6.12.1\\lib\\package.conf.d"
+DIR = "E:\\ghc\\ghc-6.12.3\\lib\\package.conf.d"
 FILES = [os.path.join(DIR, f) for f in os.listdir(DIR) if f.endswith("conf")]
 
 for line in fileinput.FileInput(FILES, inplace=1):
-    print line.replace("E:\\\\Program Files (x86)\\\\Haskell", "$topdir\\\\extralibs"),
+    print line.replace("E:\\\\Program Files (x86)\\\\Haskell", "$topdir\\\\lib\\\\extralibs"),
