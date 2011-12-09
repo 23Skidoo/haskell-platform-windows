1e946cdc867ad28ced69c7ac68e40b6b75f0b048
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2011-12-09 01:23:41 +0100

    Update the recache.py script.

--- recache.py
+++ recache.py
@@ -4,9 +4,12 @@ import os
 import os.path
 import fileinput
 
-DIR = "E:\\ghc\\ghc-7.0.1\\lib\\package.conf.d"
+DIR = "E:\\ghc\\ghc-7.0.4\\lib\\package.conf.d"
 FILES = [os.path.join(DIR, f) for f in os.listdir(DIR) if f.endswith("conf")]
 
 for line in fileinput.FileInput(FILES, inplace=1):
     print line.replace("E:\\\\Program Files (x86)\\\\Haskell",
-                       "$topdir\\\\extralibs"),
+                       "$topdir\\\\extralibs").replace(
+        "E:\\\\Documents and Settings\\\\hatemachine\\\\Application Data" \
+            "\\\\cabal",
+        "$topdir\\\\extralibs"),
