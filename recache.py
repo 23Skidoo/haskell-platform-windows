1e5c8ba73152415fc2b93b743ec74acb394818fa
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2012-05-31 07:39:12 +0200

    Paths.

--- recache.py
+++ recache.py
@@ -5,13 +5,13 @@ import os.path
 import fileinput
 
 # Change this to the appropriate version.
-DIR = "E:\\ghc\\ghc-7.0.4\\lib\\package.conf.d"
+DIR = "C:\\ghc\\ghc-7.4.1\\lib\\package.conf.d"
 
 FILES = [os.path.join(DIR, f) for f in os.listdir(DIR) if f.endswith("conf")]
 
 for line in fileinput.FileInput(FILES, inplace=1):
-    print line.replace("E:\\\\Program Files (x86)\\\\Haskell",
+    print line.replace("C:\\\\Program Files (x86)\\\\Haskell",
                        "$topdir\\\\extralibs").replace(
-        "E:\\\\Documents and Settings\\\\hatemachine\\\\Application Data" \
+        "C:\\\\Documents and Settings\\\\hatemachine\\\\Application Data" \
             "\\\\cabal",
         "$topdir\\\\extralibs"),
